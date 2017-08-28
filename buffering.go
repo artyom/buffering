33166530edfb9ed321b37b8b709f7fbe6a4c4b1a
Artyom Pervukhin <artyom@evasive.ru>
2017-08-28 22:46:22 +0300

    Add shortcut for requests without bodies

--- buffering.go
+++ buffering.go
@@ -90,6 +90,10 @@ func logFunc(r *http.Request) func(format string, v ...interface{}) {
 }
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.ContentLength == 0 {
+		h.Handler.ServeHTTP(w, r)
+		return
+	}
 	if h.maxSize > 0 && r.ContentLength > h.maxSize {
 		wErr(w, http.StatusRequestEntityTooLarge)
 		return
